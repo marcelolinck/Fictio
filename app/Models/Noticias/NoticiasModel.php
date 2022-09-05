512558e6b110984328017522481e42ee1cf090ef
marcelolinck <40663647+marcelolinck@users.noreply.github.com>
2022-09-05 10:55:58 -0400

    Ajustado status da noticia via api

--- app/Models/Noticias/NoticiasModel.php
+++ app/Models/Noticias/NoticiasModel.php
@@ -4,10 +4,20 @@ namespace App\Models\Noticias;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use PhpParser\Node\Expr\FuncCall;
+use App\Models\Noticias\NoticiasStatusModel;
 
 class NoticiasModel extends Model
 {
     use HasFactory;
 
     protected $table = "noticias";
+
+    public function status(){
+        return $this->hasOne(NoticiasStatusModel::class, 'id', 'noticia_status_id');
+    }
+
+    public function comentarios(){
+        return $this->hasMany(NoticiasComentariosModel::class, '','');
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -18,6 +19,13 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    
+    
+});
+
+Route::resource('/noticias', \App\Http\Controllers\Noticias\NoticiasController::class);
 
 Route::get('/users', function () {
     return User::all();
