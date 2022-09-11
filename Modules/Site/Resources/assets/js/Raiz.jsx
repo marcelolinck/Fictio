e9ac573a09dd765e989ab3dd226e1187dce3293e
B00TT3R <boott3r@gmail.com>
2022-09-11 14:59:45 -0400

    preparando footer e roteador

--- Modules/Site/Resources/assets/js/Raiz.jsx
+++ Modules/Site/Resources/assets/js/Raiz.jsx
@@ -1,14 +1,29 @@
-import Navbar from './components/Navbar/Navbar.jsx';
-import Conteudo from './components/Conteudo/Index.jsx';
-import Drawer from './components/Drawer/Index.jsx';
+import Navbar from './components/Template/Navbar/Navbar.jsx';
+import Conteudo from './components/Template/Conteudo/Index.jsx';
+import Drawer from './components/Template/Drawer/Index.jsx';
+import Footer from './components/Template/Footer/Index.jsx';
 import {useState, useEffect} from 'react';
+import Home from './components/Home/Index.jsx';
+/* react router stuff */
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+  } from "react-router-dom";
 function Raiz(){
     const [drawerState, setDrawerState] = useState(false);
     return(
         <Conteudo>
             <Drawer aberto={drawerState} setDrawer={setDrawerState}/>
             <Navbar setDrawer={setDrawerState}/>
-            <div style={{height:'140vh'}}></div>
+            {/* <div style={{height:'140vh'}}></div> */}
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' exact component={<Home/>}/>
+                </Routes>
+            </BrowserRouter>
+
+            <Footer />
             
         </Conteudo>
         
--- Modules/Site/Resources/assets/js/components/Conteudo/Index.jsx
+++
@@ -1,9 +0,0 @@
-import { ConteudoWrapper } from "./Styles";
-function Conteudo({ children }) {
-    return (
-        <ConteudoWrapper>
-            {children}
-        </ConteudoWrapper>
-    );
-}
-export default Conteudo;
