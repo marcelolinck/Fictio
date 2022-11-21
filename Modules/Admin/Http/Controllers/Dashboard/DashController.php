fd7f4504f9007b40c3b894df83ae092054a42282
marcelolinck <40663647+marcelolinck@users.noreply.github.com>
2022-11-20 23:50:22 -0400

    Ajuste de usuario bloqueado

--- Modules/Admin/Http/Controllers/Dashboard/DashController.php
+++ Modules/Admin/Http/Controllers/Dashboard/DashController.php
@@ -5,6 +5,7 @@ namespace Modules\Admin\Http\Controllers\Dashboard;
 use App\Models\Noticias\NoticiasComentariosModel;
 use App\Models\Noticias\NoticiasModel;
 use App\Models\User;
+use Illuminate\Auth\Events\Login;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
@@ -21,10 +22,17 @@ class DashController extends Controller
     {
         $this->title = 'Fictio';
         
+        
     }
     public function index()
     {
-        if(Auth::id() == User::where('status_id', 2)){}
+        $user = User::find(Auth::id());
+
+        if($user->status_id != 1) {
+            Auth::logout();
+            return redirect()->route('login')->with('error', 'Conta suspensa, fale com o Admin');
+        }
+        
         $config['title'] = $this->title;
         $config['namePage'] = "Dashboard de Noticias";
         $config['controller'] = 'dash';
